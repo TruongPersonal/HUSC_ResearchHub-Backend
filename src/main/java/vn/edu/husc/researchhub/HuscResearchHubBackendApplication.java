f544ac1950c184b96ee48be0f4567ef340facfa4
Ngô Quang Trường <ngoquangtruong.isme@gmail.com>
2025-07-05 13:16:21 +0700

    Force Update

---
+++ src/main/java/vn/edu/husc/researchhub/HuscResearchHubBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.husc.researchhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuscResearchHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuscResearchHubBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/husc/researchhub/HuscResearchHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.husc.researchhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuscResearchHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
