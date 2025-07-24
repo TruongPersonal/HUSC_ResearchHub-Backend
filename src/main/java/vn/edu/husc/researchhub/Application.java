55eff13df3f79d60154158fd95f6120d9bdd709c
Ngô Quang Trường <ngoquangtruong.isme@gmail.com>
2025-07-24 21:34:49 +0700

    Prefix

--- src/main/java/vn/edu/husc/researchhub/Application.java
+++ src/main/java/vn/edu/husc/researchhub/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HuscResearchHubBackendApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(HuscResearchHubBackendApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
